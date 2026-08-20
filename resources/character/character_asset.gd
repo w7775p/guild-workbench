0a06dd3c2da9a20aae01f0be68bdb2e30c14b19f
w7775p <2300101470@qq.com>
2026-08-20 21:29:45 +0800

    Add character resource base

---
+++ resources/character/character_asset.gd
@@ -0,0 +1,8 @@
+class_name CharacterAsset
+extends Resource
+
+@export var id: String
+@export var character_name: String
+@export var battle: int
+@export var investigation: int
+@export var negotiation: int
