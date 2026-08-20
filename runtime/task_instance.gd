02cc6d9f67afba422e19052c2ca0b279376de611
w7775p <2300101470@qq.com>
2026-08-20 21:29:52 +0800

    Add task runtime instance

---
+++ runtime/task_instance.gd
@@ -0,0 +1,8 @@
+class_name TaskInstance
+extends RefCounted
+
+var instance_id: String
+var task_asset_id: String
+var lifecycle_state: String = "created"
+var dispatch_id: String
+var final_result_id: String
