93fc4377b70fafc7980cd52a4367d04e70b3d1b1
w7775p <2300101470@qq.com>
2026-08-20 21:29:48 +0800

    Add task resource base

---
+++ resources/task/task_asset.gd
@@ -0,0 +1,9 @@
+class_name TaskAsset
+extends Resource
+
+@export var id: String
+@export var title: String
+@export var description: String
+@export var repeatable: bool
+@export var result_group: Resource
+@export var event_reference: Resource
