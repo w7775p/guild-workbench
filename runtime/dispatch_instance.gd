99804ae30610405c1efa9470dd34a9ea9f611f6f
w7775p <2300101470@qq.com>
2026-08-20 21:29:55 +0800

    Add dispatch runtime instance

---
+++ runtime/dispatch_instance.gd
@@ -0,0 +1,7 @@
+class_name DispatchInstance
+extends RefCounted
+
+var dispatch_instance_id: String
+var task_instance_id: String
+var character_refs: Array[String] = []
+var status: String = "created"
